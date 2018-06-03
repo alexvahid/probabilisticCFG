b114690c43456d6689232aea3af4285115cfa703
alexvahid <avahid@bu.edu>
2018-06-03 03:29:46 -0700

    Added function support. Still no recursion.

--- webPPLCFG.js
+++ webPPLCFG.js
@@ -1,13 +1,18 @@
 'use strict';
 
-//const readline = require('readline');
+/*
+NOTE: All vars must have unique names no matter scope. No variable shadowing. 
+*/
+
 const fs = require('fs');
 const Esprima = require('esprima');
+const Escodegen = require('escodegen')
 const Styx = require('styx');
 const digraphe = require('digraphe');
 const cytosnap = require('cytosnap');
-
+var AstPreprocessing = require("./node_modules/styx/lib/parser/preprocessing/functionExpressionRewriter");
 var snap = cytosnap();
+var walk = require( 'estree-walker' ).walk;
 
 
 function runWebPPL() {
@@ -22,7 +27,7 @@ function inferProbability(edge) {
     let program = "var m = function() {\n";
 
     edge.code.forEach(function(blockLevel) {
-        edge.code.forEach(function(line) {
+        blockLevel.forEach(function(line) {
             program += "\t" + line + ";\n";
         });
     });
@@ -55,33 +60,43 @@ function inferProbability(edge) {
     return trueProb;
 }
 
-fs.readFile('./tests/if_else.wppl', 'utf8', function(err, code) {
-    if (err) {
-        console.log(err);
-        return;
-    }
-
-    //create the ast
-    let ast = Esprima.parse(code);
-
-    //create the plain control flow graph
-    let controlFlowInfo = Styx.parse(ast);
-
-    //load control flow graph into graph structure
-    var graph = new digraphe();
-    controlFlowInfo.flowGraph.nodes.forEach(function(node) {
-        graph.addNode(node.id.toString(), node);
+function rebuildFunc(funcCFI) {
+    //lets assume the first node is the start
+    digraphe.Visitor.BFS(funcCFI, funcCFI.flowGraph.nodes[0].id.toString(), function (array_of_nodes, depth) {
     });
-    controlFlowInfo.flowGraph.edges.forEach(function(edge) {
-        graph.addEdge(edge.source.id.toString(), edge.target.id.toString());
+}
+
+var defaultFuncs = ['flip']
+function checkForCallExp(ast, rewrittenProgram, controlFlowInfo, incomingProbability, incomingCode, incomingConditions, blockLevel) {
+    walk(ast, {
+        enter: function ( node, parent ) {
+            if (node.type === 'CallExpression' && !defaultFuncs.includes(node.callee.name)) {   
+                let funcCFI = controlFlowInfo.functions.find(x => x.varName === node.callee.name);    
+                let graph = loadGraph(funcCFI.flowGraph);
+                visitCFG(graph,funcCFI.flowGraph.entry.id.toString(), rewrittenProgram, controlFlowInfo, incomingProbability, incomingCode, incomingConditions, blockLevel, node.arguments)                      
+            }
+        },
+        leave: function ( node, parent ) {
+            // some code happens
+        }
     });
+}
+
+function visitCFG(graph, head, rewrittenProgram, controlFlowInfo, initialProbability, incomingCode, incomingConditions, incomingBlockLevel, args) {
 
     //visit the graph
-    let blockLevel = 0;
-    let reVisit = true;
-    while(reVisit) {
+    let reVisit = false;
+    do {
+
+        let blockLevel = incomingBlockLevel;
+
+        let reVisiting = false;
+        if (reVisit) {
+            reVisiting = true;
+        }
+
         reVisit = false;
-        digraphe.Visitor.BFS(graph, '1', function (array_of_nodes, depth) {
+        digraphe.Visitor.BFS(graph, head, function (array_of_nodes, depth) {
             console.log(depth);
             array_of_nodes.forEach(function(node) {  
                 let nodeReady = true; 
@@ -98,25 +113,21 @@ fs.readFile('./tests/if_else.wppl', 'utf8', function(err, code) {
                     console.log(edge.source.id + " --> " + edge.target.id + ": " + edge.label);
                 });
 
-                //if this has been visited before
+                /*if this has been visited before
                 node.object.outgoingEdges.forEach(function(edge) {
                     if (!(typeof edge.probability === 'undefined')) {
                         nodeVisited = true;
                     } 
-                });
+                });*/
 
                 //skip this iteration if not ready or not visited
                 if (!nodeReady || nodeVisited) return;
 
-                let incomingProbability = null;
-                let incomingCode = null;
-                let incomingConditions = null;
+                let incomingProbability = 0;
 
                 //calculate the incoming total prob to this node
                 if (incomingEdges == 0) {
-                    incomingProbability = 1;
-                    incomingCode = [];
-                    incomingConditions = [];
+                    incomingProbability = initialProbability;
                 } else if (incomingEdges == 1) {
                     let incomingEdge0 = node.object.incomingEdges[0]
 
@@ -152,10 +163,31 @@ fs.readFile('./tests/if_else.wppl', 'utf8', function(err, code) {
                         incomingCode.push([]);
                     }
 
-                    code = outgoingEdge0.label;
-                    if (code != '' && outgoingEdge0.data.type == 'VariableDeclarator') {
+                    let code = outgoingEdge0.label;
+                    if (code != '' && (outgoingEdge0.data.type == 'VariableDeclarator' || outgoingEdge0.data.type == 'AssignmentExpression')) {
                         code = "var " + code;
+                        var funcRegex = /\$\$func[0-9]+/;
+                        var match = code.match(funcRegex);
+                        if(match != null) {
+                            let func = match[0];
+                            let funcCFI = controlFlowInfo.functions.find(x => x.name === func);
+                            funcCFI.varName = outgoingEdge0.data.id.name;
+                            let funcAST = rewrittenProgram.body.find(x => x.id.name === func);
+                            let funcCode = Escodegen.generate(funcAST);
+                            funcCode = funcCode.replace(funcRegex, "");
+                            code = code.replace(funcRegex, funcCode);
+                        }
+
+                        var paramRegex = /\$\$params\[([0-9]+)\]/;
+                        match = paramRegex.exec(code);
+                        if(match != null) {
+                            let paramIndex = match[1];
+                            let paramCode = Escodegen.generate(args[paramIndex]);
+                            code = code.replace(paramRegex, paramCode);
+                        }
                     }
+
+                    checkForCallExp(outgoingEdge0.data, rewrittenProgram, controlFlowInfo, incomingProbability, incomingCode, incomingConditions, blockLevel);
 
                     incomingCode[blockLevel].push(code);
                     outgoingEdge0.code = incomingCode;
@@ -177,8 +209,24 @@ fs.readFile('./tests/if_else.wppl', 'utf8', function(err, code) {
                     outgoingEdge1.conditions = incomingConditions.slice();
                     outgoingEdge1.conditions.push(outgoingEdge1.label);
 
-                    outgoingEdge0.probability = inferProbability(outgoingEdge0);
-                    outgoingEdge1.probability = inferProbability(outgoingEdge1);
+                    let inferredProbability0 = inferProbability(outgoingEdge0);
+                    let inferredProbability1 = inferProbability(outgoingEdge1);
+
+                    if (outgoingEdge0.probability && !reVisiting) {
+                        //use addition rule                     
+                        outgoingEdge0.probability = (outgoingEdge0.probability + inferredProbability0) - (outgoingEdge0.probability * inferredProbability0);        
+                    } else {
+                        outgoingEdge0.probability = inferredProbability0 * initialProbability;
+                    }
+                    if (outgoingEdge1.probability && !reVisiting) {
+                        //use addition rule                     
+                        outgoingEdge1.probability = (outgoingEdge1.probability + inferredProbability1) - (outgoingEdge1.probability * inferredProbability1);  
+                    } else {
+                        outgoingEdge1.probability = inferredProbability1 * initialProbability;
+                    }
+
+                    checkForCallExp(outgoingEdge0.data, rewrittenProgram, controlFlowInfo, incomingProbability, incomingCode, incomingConditions, blockLevel);
+                    checkForCallExp(outgoingEdge1.data, rewrittenProgram, controlFlowInfo, incomingProbability, incomingCode, incomingConditions, blockLevel);
 
                 } else {
                     console.log("error");
@@ -187,10 +235,41 @@ fs.readFile('./tests/if_else.wppl', 'utf8', function(err, code) {
 
             });
         });
+    } while(reVisit);
+}
+
+function loadGraph(flowGraph) {
+    //load control flow graph into graph structure
+    let graph = new digraphe();
+    flowGraph.nodes.forEach(function(node) {
+        graph.addNode(node.id.toString(), node);
+    });
+    flowGraph.edges.forEach(function(edge) {
+        graph.addEdge(edge.source.id.toString(), edge.target.id.toString());
+    });
+
+    return graph;
+}
+
+fs.readFile('./tests/function_recursion.wppl', 'utf8', function(err, code) {
+    if (err) {
+        console.log(err);
+        return;
     }
+
+    //create the ast
+    let ast = Esprima.parse(code);
+
+    //create the plain control flow graph
+    let controlFlowInfo = Styx.parse(ast);
+
+    var rewrittenProgram = AstPreprocessing.rewriteFunctionExpressions(ast);
+
+    let graph = loadGraph(controlFlowInfo.flowGraph);
+
+    visitCFG(graph, controlFlowInfo.flowGraph.entry.id.toString(), rewrittenProgram, controlFlowInfo, 1, [], [], 0);
     
     let controlFlowJSON = Styx.exportAsJson(controlFlowInfo);
-
     console.log(controlFlowJSON);
 
     var elements = {nodes: [], edges: []};
@@ -199,6 +278,14 @@ fs.readFile('./tests/if_else.wppl', 'utf8', function(err, code) {
     });
     controlFlowInfo.flowGraph.edges.forEach(function(edge) {
         elements.edges.push({data: { source: edge.source.id.toString(), target: edge.target.id.toString(), name: edge.label + ", " + edge.probability }});
+    });
+    controlFlowInfo.functions.forEach(function(func) {
+        func.flowGraph.nodes.forEach(function(node) {
+            elements.nodes.push({data: { id: node.id.toString() }})
+        });
+        func.flowGraph.edges.forEach(function(edge) {
+            elements.edges.push({data: { source: edge.source.id.toString(), target: edge.target.id.toString(), name: edge.label + ", " + edge.probability }});
+        });
     });
 
     snap.start().then(function(){
@@ -211,28 +298,27 @@ fs.readFile('./tests/if_else.wppl', 'utf8', function(err, code) {
             style: [
                 {
                 selector: 'node',
-                style: {
-                    'content': 'data(id)',
-                    'text-valign': 'center',
-                    'text-halign': 'middle',
-                    'background-color': '#11479e',
-                    'color' : 'white',
-                }
-                },
-            
+                    style: {
+                        'content': 'data(id)',
+                        'text-valign': 'center',
+                        'text-halign': 'middle',
+                        'background-color': '#11479e',
+                        'color' : 'white',
+                    }
+                },        
                 {
-                selector: 'edge',
-                style: {
-                    'content': 'data(name)',
-                    'text-opacity': 0.5,
-                    'text-valign': 'bottom',
-                    'text-halign': 'right',
-                    'curve-style': 'bezier',
-                    'width': 4,
-                    'target-arrow-shape': 'triangle',
-                    'line-color': '#9dbaea',
-                    'target-arrow-color': '#9dbaea'
-                }
+                    selector: 'edge',
+                    style: {
+                        'content': 'data(name)',
+                        'text-opacity': 0.5,
+                        'text-valign': 'bottom',
+                        'text-halign': 'right',
+                        'curve-style': 'bezier',
+                        'width': 4,
+                        'target-arrow-shape': 'triangle',
+                        'line-color': '#9dbaea',
+                        'target-arrow-color': '#9dbaea'
+                    }
                 }
             ],
             
